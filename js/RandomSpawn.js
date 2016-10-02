627565d0b6cfa8572792965bfb521f85c09479a2
Kyle Ring <ringk2@rpi.edu>
2016-10-02 19:23:09 -0400

    Added "RandomSpawn" call into game.html. As of now, no effect on game space, merely counts out delay time. Set ground for later use

--- js/RandomSpawn.js
+++ js/RandomSpawn.js
@@ -1,15 +1,26 @@
-var start_delay;
-var next_delay
 
-function RandomSpawn () {
+var next_delay;
 
-	next_delay = GetRandomNum(1000, 4500); //Min and Max in milliseconds (1000 Milliseconds = 1 second)
+var SelectedBuilding;
 
-	console.log(next_delay);
+function RandomSpawn () { //Finished code should take in an array such as BuildingOptions[] containing the objects/images it can choose between
 
-	setTimeout(RandomSpawn, next_delay); //Set the function to be called again after the random delay passes
+	//Randomly determine a delay between calls, and call the function after that delay passes
+
+	next_delay = GetRandomNum(1000, 3500); //Min and Max in milliseconds (1000 Milliseconds = 1 second)
+	next_delay = Math.round(next_delay); //Round the delay to the nearest int
+
+	console.log(next_delay); //Debug statement, outputs random delay
+
+	//Insert Code here to handle selection of background object, as SelectedBuilding.
+
+	setTimeout(RandomSpawn, next_delay /*, BuildingOptions */); //Set the function to be called again after the random delay passes
+
+
+	//Code to Insert SelectedBuilding into gamespace. Should also apply anything such as movement properties to scroll with screen.
+
 }
 
-function GetRandomNum (min, max) {
+function GetRandomNum (min, max) { //This function handles finding a random number in a given min max range.
 	return Math.random() * (max - min) + min; //Return a random number within the range of min and max
 }
