315ae652093e90c361f9a89224b8e2fd70819ea5
Jordan Cubbin <cubbinjordan>
2016-10-03 17:10:50 -0400

    Add item loading and collisions
    Including labels for score and lives, and depending on which item you
    hit, those values are changed

--- js/game.js
+++ js/game.js
@@ -8,6 +8,14 @@ window.onload = function() {
 
 	var player, dragMagnitude = 500, boatSpeed = 600, slowDist = 200,
 		scrollSpeed = 5;
+
+	var items;
+	var rocks;
+
+	var score=0;
+	var labelScore;
+	var lives=3;
+	var labelLives;
 	
 	var world, bgWalls,
 		bgKeys = ['bg1', 'bg2', 'bg3',
@@ -21,6 +29,12 @@ window.onload = function() {
 		//load images
 		game.load.image('player', 'player.png')
 		         .images(bgKeys);
+
+		game.load.image ('rock', 'bullet.png');
+		game.load.image ('mem', 'star.png');
+		game.load.image ('bro', 'einstein.png')
+		game.load.spritesheet ('dudeBroRaft', 'dude.png', 32, 48);      
+
 	}
 
 	function create () {
@@ -34,7 +48,7 @@ window.onload = function() {
 		game.world.bounds.y = bgWalls.minHeight;
 
 		//make a player thing
-		player = game.add.sprite(200,200, 'player');
+		player = game.add.sprite(200,200, 'dudeBroRaft	');
 
 		game.physics.enable(player, Phaser.Physics.ARCADE);
 		
@@ -44,7 +58,39 @@ window.onload = function() {
 
 		player.body.drag.x = Math.sqrt(2) * dragMagnitude;
 		player.body.drag.y = Math.sqrt(2) * dragMagnitude;
+		
+		//create memorabilia items
 
+		items = game.add.group(world);
+		items.enableBody = true;
+
+		//create rock
+		rocks = game.add.group(world);
+		rocks.enableBody = true;
+
+		//create bro
+		bros = game.add.group(world);
+		bros.enableBody = true;
+
+		//create all the objects
+		for(var i=0;i<12;i++){
+			//mems
+			var item = items.create(900,i*240,'mem');
+			item.body.velocity.x = -100;
+			//rocks
+			var rock = rocks.create(900,80+i*240,'rock');
+			rock.body.velocity.x = -100;
+			//bros
+			var bro = bros.create(900,160+i*240,'bro')
+			bro.body.velocity.x = -100;
+		}
+
+		//score label
+		var style = {font: "32px Arial", fill: "#1100ff", align: "center"};
+		var text = score;
+		labelScore = game.add.text (100, 500, text, style);
+		text = lives;
+		labelLives = game.add.text (300, 500, text, style);
 	}
 
 
@@ -79,6 +125,54 @@ window.onload = function() {
 
 			}
 		}
+
+		//collectable code
+		//collisions
+		game.physics.arcade.overlap(player, items, collectItem, null, this);
+		game.physics.arcade.overlap(player, bros, broPickup, null, this);
+		game.physics.arcade.overlap(player, rocks, rockHit, null, this);
+
+		//player drag
+		player.body.drag.x = 1000;
+		player.body.drag.y = 1000;
+
+		//move player
+		if(cursors.left.isDown)
+		{
+			player.body.velocity.x = -200;
+		}
+		else if (cursors.right.isDown){
+			player.body.velocity.x = 200;
+		}
+
+		if(cursors.up.isDown)
+		{
+			player.body.velocity.y = -200;
+		}
+		else if (cursors.down.isDown)
+		{
+			player.body.velocity.y = 200;
+		}
+
+		//update score
+		labelScore.text = score;
+		labelLives.text = lives;
+
+	}
+
+	function collectItem(thisPlayer, thisItem){
+		thisItem.kill();
+		score += 1;
+	}
+
+	function broPickup(thisPlayer, thisBro){
+		thisBro.kill();
+		lives += 1;
+	}
+
+	function rockHit(thisPlayer, thisRock){
+		thisRock.kill();
+		lives -= 1;
 	}
 
 };
