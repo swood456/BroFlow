478f664892426f10e8e37ab46b77d89f10ef0975
swood456 <crazedsportsfan@gmail.com>
2016-10-11 22:20:43 -0400

    fixed some stuff

--- js/game.js
+++ js/game.js
@@ -177,11 +177,12 @@ window.onload = function() {
 		
 		//add in some groups
 		world    = game.add.group();
+		deadbros = game.add.group(world, undefined, false, true, Phaser.Physics.ARCADE);
 
 		//add in spawners
 		bgWalls  = new BGWalls(game, world, bgKeys, skyKeys);
 		items    = new Spawner(game, world, ['pickup1'], 600, 1000, bgWalls.minHeight, game.height - (game.cache.getImage('pickup1').height));
-		deadbros = game.add.group(world, undefined, false, true, Phaser.Physics.ARCADE);
+		
 		rocks    = new Spawner(game, world, ['boulder', 'bricks'], 1800, 2000, bgWalls.minHeight, game.height - (game.cache.getImage('boulder').height / 2));
 		bros     = new Spawner(game, world, ['broLife2'], 500, 900, bgWalls.minHeight, game.height - (game.cache.getImage('broLife2').height));
 		powerups = new Spawner(game, world, ['powerup'], 15000, 20000, bgWalls.minHeight, game.height - (game.cache.getImage('powerup').height));
